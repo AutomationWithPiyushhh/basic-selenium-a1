819990240f906a432cd362c488ce5ba3c502d531
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2026-06-24 14:54:50 +0530

    select started

---
+++ basic-selenium-a1/src/test/java/learning_dd/AWP_select.java
@@ -0,0 +1,38 @@
+package learning_dd;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AWP_select {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		driver.get("https://automationwithpiyush.vercel.app/dropdown.html");
+		
+//		handle single select dropdown
+		WebElement ssDD = driver.findElement(By.id("single-select"));
+		Select selSingle = new Select(ssDD);
+		selSingle.selectByIndex(5);
+		
+
+//		handle multi select dropdown
+		WebElement msDD = driver.findElement(By.id("multi-select"));
+		Select selMulti = new Select(msDD);
+		selMulti.selectByVisibleText("Python");
+		selMulti.selectByVisibleText("Git");
+		selMulti.selectByVisibleText("Maven");
+		
+		
+		
+		
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
